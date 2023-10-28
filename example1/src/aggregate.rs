98b774109421e015ae072a1f4c329f55d00dc981
Eveheeero <xhve00000@naver.com>
2023-10-28 12:04:29 +0000

    +

--- example1/src/aggregate.rs
+++ example1/src/aggregate.rs
@@ -1,15 +1,18 @@
 use ruva::prelude::*;
 
-#[derive(Default, Aggregate)]
+#[aggregate]
+#[derive(Default, Serialize, Deserialize)]
 pub(super) struct MyObjects {
+	#[identifier]
+	pub(crate) id: usize,
 	remain_coffee: usize,
 	remain_tea: usize,
-	pub(crate) events: std::collections::VecDeque<std::boxed::Box<dyn Message>>,
+	// pub(crate) events: std::collections::VecDeque<std::boxed::Box<dyn Message>>,
 }
 
 impl MyObjects {
 	pub(crate) fn do_work(&self) {
-		self.raise_event(event)
+		// self.raise_event(event)
 		// -> uow가 커밋할때 레포지토리에 전달됨, 레포지토리가 나중에 처리함
 	}
 }
